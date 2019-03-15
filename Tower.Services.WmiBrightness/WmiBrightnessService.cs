008f35ef9263dd1c166c6e08b186baca8c7aed4c
Alex Noddings <alexnoddings101@gmail.com>
2019-03-15 02:49:13 +0000

    Added a Windows Management Instrumentation (WMI)-based brightness service. Will noop if not available.

---
+++ Tower.Services.WmiBrightness/WmiBrightnessService.cs
@@ -0,0 +1,22 @@
+﻿using Tower.Core.Services;
+
+namespace Tower.Services.WmiBrightness
+{
+    public class WmiBrightnessService : IBrightnessService
+    {
+        private readonly bool _canUseWmiBrightness;
+
+        public WmiBrightnessService()
+        {
+            _canUseWmiBrightness = WmiHelper.CanBeUsed();
+        }
+
+        public bool SetBrightness(double brightnessPercent)
+        {
+            if (!_canUseWmiBrightness) return false;
+
+            WmiHelper.SetBrightness(brightnessPercent);
+            return true;
+        }
+    }
+}
