b1376ca3f8ae2b841cf0475b8c3992aeee90089b
Alex Noddings <alexnoddings101@gmail.com>
2019-03-15 21:49:47 +0000

    Added a current media progress bar user control

---
+++ Tower.Application/Views/Music/MusicCurrentMediaProgressBarView.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+
+namespace Tower.Application.Views.Music
+{
+    /// <summary>
+    /// Interaction logic for MusicCurrentMediaProgressBarView.xaml
+    /// </summary>
+    public partial class MusicCurrentMediaProgressBarView
+    {
+        public static readonly DependencyProperty PercentProperty = DependencyProperty.Register("Percent",
+            typeof(double), typeof(MusicCurrentMediaProgressBarView), new PropertyMetadata(PercentChangedCallback));
+
+        private static void PercentChangedCallback(object sender, DependencyPropertyChangedEventArgs e) =>
+            ((MusicCurrentMediaProgressBarView)sender).PercentChanged(e.NewValue);
+
+        public double Percent
+        {
+            get => (double)GetValue(PercentProperty);
+            set => SetValue(PercentProperty, value);
+        }
+
+        private void PercentChanged(object parameter)
+        {
+            if (parameter is double percent)
+                Width = (percent / 100) * MaxWidth;
+        }
+
+        public MusicCurrentMediaProgressBarView()
+        {
+            InitializeComponent();
+        }
+    }
+}
