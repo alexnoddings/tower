2c7e5e84cbc647e3260b1cea209c3bac0a0943c6
Alex Noddings <alexnoddings101@gmail.com>
2019-03-15 23:39:15 +0000

    Added semi-functional settings menu to MainWindow

--- Tower.Application/ViewModel/MainViewModel.cs
+++ Tower.Application/ViewModel/MainViewModel.cs
@@ -1,36 +1,82 @@
 using System;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 
 namespace Tower.Application.ViewModel
 {
-    /// <summary>
-    /// This class contains properties that the main View can data bind to.
-    /// <para>
-    /// Use the <strong>mvvminpc</strong> snippet to add bindable properties to this ViewModel.
-    /// </para>
-    /// <para>
-    /// You can also use Blend to data bind with the tool's support.
-    /// </para>
-    /// <para>
-    /// See http://www.galasoft.ch/mvvm
-    /// </para>
-    /// </summary>
     public class MainViewModel : ViewModelBase
     {
+        private bool _isDayMode;
+        public bool IsDayMode
+        {
+            get => _isDayMode;
+            set => HandlePropertyChange(ref _isDayMode, value);
+        }
+
+        private bool _isSettingsVisible;
+        public bool IsSettingsVisible
+        {
+            get => _isSettingsVisible;
+            set => HandlePropertyChange(ref _isSettingsVisible, value);
+        }
+
+        public RelayCommand ToggleSettingsCommand { get; }
+        public RelayCommand ToggleDayModeCommand { get; }
+        public RelayCommand OpenAlarmsCommand { get; }
+        public RelayCommand OpenBackgroundsCommand { get; }
+        public RelayCommand CloseApplicationCommand { get; }
+
         /// <summary>
         /// Initializes a new instance of the MainViewModel class.
         /// </summary>
         public MainViewModel()
         {
-            ////if (IsInDesignMode)
-            ////{
-            ////    // Code runs in Blend --> create design time data.
-            ////}
-            ////else
-            ////{
-            ////    // Code runs "for real"
-            ////}
+            if (IsInDesignMode)
+            {
+                IsSettingsVisible = true;
+            }
+            else
+            {
+                IsSettingsVisible = false;
+            }
+
+            IsDayMode = true;
+
+            ToggleSettingsCommand = new RelayCommand(() => IsSettingsVisible = !IsSettingsVisible);
+            ToggleDayModeCommand = new RelayCommand(() => { IsSettingsVisible = false; ToggleDayMode(); });
+            OpenAlarmsCommand = new RelayCommand(() => { IsSettingsVisible = false; OpenAlarms(); });
+            OpenBackgroundsCommand = new RelayCommand(() => { IsSettingsVisible = false; OpenBackgrounds(); });
+            CloseApplicationCommand = new RelayCommand(CloseApplication);
+        }
+
+        private void ToggleDayMode()
+        {
+            IsDayMode = !IsDayMode;
+        }
+
+        private void OpenAlarms()
+        {
+
+        }
+
+        private void OpenBackgrounds()
+        {
+
+        }
+
+        private void CloseApplication()
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+
+        private bool HandlePropertyChange<T>(ref T field, T value, [CallerMemberName] string propertyName = "")
+        {
+            if (value.Equals(field)) return false;
+            field = value;
+            RaisePropertyChanged(propertyName);
+            return true;
         }
     }
 }
