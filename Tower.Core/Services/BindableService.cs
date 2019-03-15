6f071fd3fd47e2969dcea77ae2729ab18b690a3e
Alex Noddings <alexnoddings101@gmail.com>
2019-03-15 19:49:04 +0000

    Updated Core to include a bindable service to reduce code duplication across service implementations

---
+++ Tower.Core/Services/BindableService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tower.Core.Services
+{
+    public abstract class BindableService : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void HandlePropertyChange<T>(ref T field, T value, [CallerMemberName] string propertyName = "") where T : IEquatable<T>
+        {
+            if (value.Equals(field)) return;
+            field = value;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
