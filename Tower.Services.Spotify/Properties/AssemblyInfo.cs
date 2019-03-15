8ef246966b7a9db532b1055f8e8e9bcbd8634ba1
Alex Noddings <alexnoddings101@gmail.com>
2019-03-15 18:48:50 +0000

    Added a spotify music service

---
+++ Tower.Services.Spotify/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tower.Services.Spotify")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("Tower.Services.Spotify")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("21e2c2d8-b5d6-4c60-a0f3-876f475de5dc")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
