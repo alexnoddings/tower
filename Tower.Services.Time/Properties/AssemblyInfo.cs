6c742f7cf50264116ea0aa1967c5edf104eb560d
Alex Noddings <alexnoddings101@gmail.com>
2019-03-15 20:12:35 +0000

    Add a TimeService

---
+++ Tower.Services.Time/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tower.Services.Time")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("Tower.Services.Time")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("964b1aee-1bda-472b-b3b6-f5ac4726b18a")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tower.Services.Time/TimeService.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Windows.Threading;
+using Tower.Core.Services;
+
+namespace Tower.Services.Time
+{
+    public class TimeService : BindableService, ITimeService
+    {
+        private static readonly TimeSpan UpdateInterval = TimeSpan.FromSeconds(2);
+
+        #region ITimeService
+        private string _timeText;
+        public string TimeText
+        {
+            get => _timeText;
+            private set => HandlePropertyChange(ref _timeText, value);
+        }
+
+        private string _dateText;
+        public string DateText
+        {
+            get => _dateText;
+            private set => HandlePropertyChange(ref _dateText, value);
+        }
+
+        private DateTime _nextAlarm;
+        public DateTime NextAlarm
+        {
+            get => _nextAlarm;
+            private set => HandlePropertyChange(ref _nextAlarm, value);
+        }
+
+        public void SetNextAlarm(TimeSpan timeOfDay)
+        {
+            if (timeOfDay <= DateTime.Now.TimeOfDay)
+                NextAlarm = DateTime.Today + TimeSpan.FromDays(1) + timeOfDay;
+            else
+                NextAlarm = DateTime.Today + timeOfDay;
+        }
+
+        public event EventHandler AlarmTriggered;
+        #endregion
+
+        public TimeService()
+        {
+            Update();
+            var updateTimer = new DispatcherTimer(DispatcherPriority.Normal) { Interval = UpdateInterval };
+            updateTimer.Tick += (_, __) => Update();
+            updateTimer.Start();
+        }
+
+        private void Update()
+        {
+            TimeText = DateTime.Now.ToString("hh:mm:ss tt zz");
+            DateText = DateTime.Now.ToString("yyyy-MM-dd");
+
+            if (NextAlarm > DateTime.Now) return;
+
+            AlarmTriggered?.Invoke(this, EventArgs.Empty);
+            NextAlarm = NextAlarm + TimeSpan.FromDays(1);
+        }
+    }
+}
