d2593df6fd6720b3472ce15268d29542013f096a
Alex Noddings <alexnoddings101@gmail.com>
2019-03-15 18:48:35 +0000

    Added a bing background service

---
+++ Tower.Services.BingBackground/BingBackgroundService.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+using Tower.Core.Services;
+
+namespace Tower.Services.BingBackground
+{
+    public class BingBackgroundService : IBackgroundService, INotifyPropertyChanged
+    {
+        private static readonly TimeSpan NewBackgroundCheckInterval = TimeSpan.FromSeconds(15);
+
+        private DateTime _lastBackgroundCheckDate = DateTime.MinValue;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string _selectedBackgroundUri;
+        public string SelectedBackgroundUri
+        {
+            get => _selectedBackgroundUri;
+            private set
+            {
+                if (value == _selectedBackgroundUri) return;
+                _selectedBackgroundUri = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private List<string> _availableBackgroundUris;
+        public List<string> AvailableBackgroundUris
+        {
+            get => _availableBackgroundUris;
+            private set
+            {
+                if (value == _availableBackgroundUris) return;
+                _availableBackgroundUris = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public BingBackgroundService()
+        {
+            SelectedBackgroundUri = "";
+            AvailableBackgroundUris = new List<string>();
+
+            var timer = new DispatcherTimer(DispatcherPriority.Background) {Interval = NewBackgroundCheckInterval};
+            timer.Tick += async (_, __) => await CheckForNewBackgroundsAsync();
+            timer.Start();
+
+            // Begin checking in the background
+            Task.Factory.StartNew(async () => await CheckForNewBackgroundsAsync());
+        }
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private async Task CheckForNewBackgroundsAsync()
+        {
+            if (DateTime.Today <= _lastBackgroundCheckDate) return;
+
+            _lastBackgroundCheckDate = DateTime.Today;
+            AvailableBackgroundUris = await BingHelper.GetImageUrisAsync();
+            SelectedBackgroundUri = AvailableBackgroundUris.First();
+        }
+    }
+}
---
+++ Tower.Services.BingBackground/BingHelper.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Tower.Services.BingBackground
+{
+    internal static class BingHelper
+    {
+        private const string UrlBase = "https://www.bing.com";
+        private const string ImagesEndpoint = "/HPImageArchive.aspx?format=xml&idx=0&n=8&mkt=en-GB";
+
+        private static readonly HttpClient Client;
+
+        static BingHelper()
+        {
+            Client = new HttpClient();
+        }
+
+        internal static async Task<List<string>> GetImageUrisAsync()
+        {
+            using (Stream stream = await Client.GetStreamAsync(UrlBase + ImagesEndpoint))
+            using (var streamReader = new StreamReader(stream))
+            {
+                string content = await streamReader.ReadToEndAsync();
+                IEnumerable<XElement> images = XElement.Parse(content).Elements("image");
+
+                return (from image in images
+                    select image.Element("urlBase")?.Value
+                    into relativeUri
+                    where relativeUri != null
+                    select $"{UrlBase}{relativeUri}_1920x1080.jpg").ToList();
+            }
+        }
+    }
+}
