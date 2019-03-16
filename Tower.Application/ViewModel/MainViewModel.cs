335411e3c217ed8eeb93749f1b617628b1f49384
Alex Noddings <alexnoddings101@gmail.com>
2019-03-16 06:18:36 +0000

    Updated MainView and MainViewModel

--- Tower.Application/ViewModel/MainViewModel.cs
+++ Tower.Application/ViewModel/MainViewModel.cs
@@ -1,18 +1,42 @@
 using System;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
+using Tower.Application.DesignTimeServices;
+using Tower.Application.Views;
+using Tower.Core.Services;
+using Tower.Services.BingBackground;
 
 namespace Tower.Application.ViewModel
 {
     public class MainViewModel : ViewModelBase
     {
+        public IBackgroundService BackgroundService { get; }
+        public ITimeService TimeService { get; }
+        private IBrightnessService _brightnessService;
+        private ISpotifyService _spotifyService;
+
         private bool _isDayMode;
         public bool IsDayMode
         {
             get => _isDayMode;
             set => HandlePropertyChange(ref _isDayMode, value);
+        }
+
+        private bool _isAlarmControlVisible;
+        public bool IsAlarmControlVisible
+        {
+            get => _isAlarmControlVisible;
+            set => HandlePropertyChange(ref _isAlarmControlVisible, value);
+        }
+
+        private bool _isBackgroundSelectionVisible;
+        public bool IsBackgroundSelectionVisible
+        {
+            get => _isBackgroundSelectionVisible;
+            set => HandlePropertyChange(ref _isBackgroundSelectionVisible, value);
         }
 
         private bool _isSettingsVisible;
@@ -22,53 +46,117 @@ namespace Tower.Application.ViewModel
             set => HandlePropertyChange(ref _isSettingsVisible, value);
         }
 
+        private UserControl _dayView;
+        private UserControl _nightView;
+        private UserControl _musicView;
+
+        private UserControl _activeUserControl;
+        public UserControl ActiveUserControl
+        {
+            get => _activeUserControl;
+            set => HandlePropertyChange(ref _activeUserControl, value);
+        }
+
         public RelayCommand ToggleSettingsCommand { get; }
         public RelayCommand ToggleDayModeCommand { get; }
-        public RelayCommand OpenAlarmsCommand { get; }
+        public RelayCommand OpenAlarmCommand { get; }
+        public RelayCommand CloseAlarmCommand { get; }
         public RelayCommand OpenBackgroundsCommand { get; }
+        public RelayCommand CloseBackgroundsCommand { get; }
         public RelayCommand CloseApplicationCommand { get; }
 
         /// <summary>
         /// Initializes a new instance of the MainViewModel class.
         /// </summary>
-        public MainViewModel()
+        public MainViewModel(IBackgroundService backgroundService, ITimeService timeService, IBrightnessService brightnessService, ISpotifyService spotifyService)
         {
             if (IsInDesignMode)
             {
+                IsAlarmControlVisible = true;
+                IsBackgroundSelectionVisible = false;
                 IsSettingsVisible = true;
             }
             else
             {
+                IsAlarmControlVisible = false;
+                IsBackgroundSelectionVisible = false;
                 IsSettingsVisible = false;
             }
+            
+            _dayView = new DayView();
+            _nightView = new NightView();
+            _musicView = new MusicView();
+            ActiveUserControl = _dayView;
 
             IsDayMode = true;
 
+            BackgroundService = backgroundService;
+            TimeService = timeService;
+            TimeService.AlarmTriggered += async (_, __) => await _spotifyService.ResumePlaybackAsync(true, 1);
+            _brightnessService = brightnessService;
+            _spotifyService = spotifyService;
+            _spotifyService.PlaybackStateModified += (_, __) => UpdateActiveUserControl();
+
             ToggleSettingsCommand = new RelayCommand(() => IsSettingsVisible = !IsSettingsVisible);
-            ToggleDayModeCommand = new RelayCommand(() => { IsSettingsVisible = false; ToggleDayMode(); });
-            OpenAlarmsCommand = new RelayCommand(() => { IsSettingsVisible = false; OpenAlarms(); });
-            OpenBackgroundsCommand = new RelayCommand(() => { IsSettingsVisible = false; OpenBackgrounds(); });
+            ToggleDayModeCommand = new RelayCommand(() => { ToggleDayMode(); });
+            OpenAlarmCommand = new RelayCommand(() => { OpenAlarm(); });
+            CloseAlarmCommand = new RelayCommand(() => { CloseAlarm(); });
+            OpenBackgroundsCommand = new RelayCommand(() => { OpenBackgroundSelection(); });
+            CloseBackgroundsCommand = new RelayCommand(() => { CloseBackgroundSelection(); });
             CloseApplicationCommand = new RelayCommand(CloseApplication);
         }
 
         private void ToggleDayMode()
         {
+            IsSettingsVisible = false;
             IsDayMode = !IsDayMode;
+            UpdateActiveUserControl();
         }
 
-        private void OpenAlarms()
+        private void OpenAlarm()
         {
-
+            IsSettingsVisible = false;
+            IsAlarmControlVisible = true;
         }
 
-        private void OpenBackgrounds()
+        private void CloseAlarm()
         {
+            IsAlarmControlVisible = false;
+        }
 
+        private void OpenBackgroundSelection()
+        {
+            IsSettingsVisible = false;
+            IsBackgroundSelectionVisible = true;
+        }
+
+        private void CloseBackgroundSelection()
+        {
+            IsBackgroundSelectionVisible = false;
         }
 
         private void CloseApplication()
         {
             System.Windows.Application.Current.Shutdown();
+        }
+
+        private void UpdateActiveUserControl()
+        {
+            if (_spotifyService.IsPlaying)
+            {
+                ActiveUserControl = _musicView;
+                _brightnessService.SetBrightness(1);
+            }
+            else if (IsDayMode)
+            {
+                ActiveUserControl = _dayView;
+                _brightnessService.SetBrightness(1);
+            }
+            else
+            {
+                ActiveUserControl = _nightView;
+                _brightnessService.SetBrightness(0.01);
+            }
         }
 
         private bool HandlePropertyChange<T>(ref T field, T value, [CallerMemberName] string propertyName = "")
